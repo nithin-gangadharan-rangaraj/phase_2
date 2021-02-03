3fd9ac52cdf5989206dd3ca7e6e6762f7f95a630
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2021-02-03 10:14:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def main():
 		#fig = sns.countplot(x='age',data = positive_cases, hue = 'TenYearCHD', palette='husl')
 		#st.pyplot(fig)
 		st.dataframe(positive_cases)
-		hist_values = np.histogram(positive_cases['age'], bins=5)[0]
+		hist_values = np.histogram(positive_cases['age'], bins=1)[0]
 		st.bar_chart(hist_values)
 		
 if __name__ == '__main__':
