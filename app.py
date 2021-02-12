6b5a71d23922610a8af9cace340917175abffb2a
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2021-02-12 12:15:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.set_page_config(**PAGE_CONFIG)
 html = '''
 <style>
 body {
-background-image: url("https://www.wallpapertip.com/wmimgs/129-1290957_pink-colored-wallpaper-solid-colors-red-light-color.jpg");
+background-image: url("https://wallpapercave.com/wp/wp2838450.jpg");
 background-size: cover;
 }
 </style>
