c7ac1a25958f2e76bf704f1104c5791b3d03e5f2
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2021-02-12 12:31:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,34 +39,31 @@ def scale_fun(data):
 
 def main():
 	st.markdown(html, unsafe_allow_html=True)
-	st.sidebar.title("Sidebar")
-	st.sidebar.write("Check the box for predicting *heart risk*")
-	if(st.sidebar.checkbox("Risk predictor")):
-		st.subheader("*Enter the following parameters for prediction*")
-		age = st.slider("Select your age", 1, 100)
-		totChol = st.number_input("Enter Cholesterol")
-		sysBP = st.number_input("Enter Systolic BP")
-		diaBP = st.number_input("Enter Diastolic BP")
-		BMI = st.number_input("Enter BMI")
-		heartrate = st.number_input("Enter Heart Rate")
-		glucose = st.number_input("Enter Glucose")
-		x = [age, totChol, sysBP, diaBP, BMI, heartrate, glucose]
-		new = np.array(scale_fun(x))
-		if(st.button("Predict")):
-			probs = clf_svm.predict_proba(new.reshape(1, -1))
-			if (probs[0][1] > probs[0][0]):
-				percentage = probs[0][1] * 100
-				st.write("You are ",round(percentage,2),"% at a risk of developing CVD!")
-			elif (probs[0][0] > probs[0][1]):
-				st.write("No worries, You are safe!")
+	st.subheader("*Enter the following parameters for prediction*")
+	age = st.slider("Select your age", 1, 100)
+	totChol = st.number_input("Enter Cholesterol")
+	sysBP = st.number_input("Enter Systolic BP")
+	diaBP = st.number_input("Enter Diastolic BP")
+	BMI = st.slider("Select your age", 1, 50)
+	heartrate = st.number_input("Enter Heart Rate")
+	glucose = st.number_input("Enter Glucose")
+	x = [age, totChol, sysBP, diaBP, BMI, heartrate, glucose]
+	new = np.array(scale_fun(x))
+	if(st.button("Predict")):
+		probs = clf_svm.predict_proba(new.reshape(1, -1))
+		if (probs[0][1] > probs[0][0]):
+			percentage = probs[0][1] * 100
+			st.write("You are ",round(percentage,2),"% at a risk of developing CVD!")
+		elif (probs[0][0] > probs[0][1]):
+			st.write("No worries, You are safe!")
 	
-	if(st.sidebar.checkbox("Age analysis")):
-		st.set_option('deprecation.showPyplotGlobalUse', False)
-		st.subheader("AGE - Statistics")
-		positive_cases = data[data['TenYearCHD'] == 1]
-		plt.figure(figsize=(15,6))
-		sns.countplot(x='age',data = positive_cases, hue = 'TenYearCHD', palette='husl')
-		st.pyplot()
+	#if(st.sidebar.checkbox("Age analysis")):
+	#	st.set_option('deprecation.showPyplotGlobalUse', False)
+	#	st.subheader("AGE - Statistics")
+	#	positive_cases = data[data['TenYearCHD'] == 1]
+	#	plt.figure(figsize=(15,6))
+	#	sns.countplot(x='age',data = positive_cases, hue = 'TenYearCHD', palette='husl')
+	#	st.pyplot()
 		
 		
 if __name__ == '__main__':
