f084f01cf7d37dd19a4922d28a507da6abfa95ec
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2021-03-22 23:14:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,6 +33,7 @@ def scale_fun(data):
 
 
 def main():
+	stt.set_theme({'primary': '#1b3388'})
 	st.markdown(page_bg_img, unsafe_allow_html=True)
 	st.subheader("*Enter the following parameters for prediction*")
 	age = st.slider("Select your age", 1, 100)
