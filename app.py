a4549be2e1c0cb5f6dd689247564632a790e36a8
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2021-03-22 18:35:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,6 +39,13 @@ def scale_fun(data):
 
 def main():
 	st.markdown(html, unsafe_allow_html=True)
+        hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+        st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 	st.subheader("*Enter the following parameters for prediction*")
 	age = st.slider("Select your age", 1, 100)
 	totChol = st.number_input("Enter Cholesterol")
